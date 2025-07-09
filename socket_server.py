320b64d6c14ada4ad55eba1d67c953e9512788e6
Colton Fox <coltonfox2004@gmail.com>
2025-07-09 15:27:01 +0000

    Setup to use env variables

--- socket_server.py
+++ socket_server.py
@@ -1,13 +1,15 @@
 """The socket IO server that sends the JSON packets"""
+import os
+import zmq
 from flask import Flask
 from flask_socketio import SocketIO
 from process_realtime import connect_to_zmq_socket, receive_socket_msg, dmap_to_json
-import zmq
+from dotenv import load_dotenv
 
-SOCKET_ADDR = "sdc-serv.usask.ca:5300" # Socket address for canada radars
+DEFAULT_HOST = '0.0.0.0'
+DEFAULT_PORT = 5000
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'secret!'
 socketio = SocketIO(app, cors_allowed_origins="*") #TODO: specify CORS policy?
 
 @socketio.on('connect')
@@ -16,7 +18,7 @@ def handle_connect():
     socketio.start_background_task(start_listening)
 
 def start_listening():
-    socket = connect_to_zmq_socket(SOCKET_ADDR)
+    socket = connect_to_zmq_socket(os.getenv('CANADA_ADDR'))
 
     while True:
         try:
@@ -33,4 +35,5 @@ def start_listening():
             print(f"Failed to create JSON packet for {site_name}, corrupt data fields")
 
 if __name__ == '__main__':
-    socketio.run(app, debug=True)
+    load_dotenv()
+    socketio.run(app, host=os.getenv('HOST', DEFAULT_HOST), port=os.getenv('PORT', DEFAULT_PORT), debug=os.getenv('DEBUG', 'false') == 'true')
